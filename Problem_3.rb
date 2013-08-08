9cc17a699540fbb6a4c54bcaec98dd9ecab3db17
Amy Lukima <amylukima@gmail.com>
2013-08-08 02:28:20 -0700

    add problem 3, add readme

---
+++ Problem_3.rb
@@ -0,0 +1,8 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+#What is a prime factor? 
+#http://en.wikipedia.org/wiki/Prime_factor
+
+#Look at fib wiki and this wiki? Is there a pattern? Maybe a formula? 
---
+++ readme.md
@@ -0,0 +1,7 @@
+##Comments
+Comments are teacing notes for myself. 
+
+##Refactorings 
+
+After every solution I will refactor the code and commit to show solution steps. 
+
