db10c77fc68d96d81b9ceacad8f19209b214e796
Amy Lukima <amylukima@gmail.com>
2013-08-22 23:11:50 -0700

    add problem 4 description

---
+++ Problem_4.rb
@@ -0,0 +1,15 @@
+=begin
+first, look at how I'm making a comment!
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+Lets start by taking a look at what a palindromic number is. 
+http://en.wikipedia.org/wiki/Palindromic_number
+
+We want to find a number that:
+is the same forward and reversed
+is the product of two numbers 
+since we want the largest number possible it seems like it makes sense to start at 999 and work our way down 
+=end
