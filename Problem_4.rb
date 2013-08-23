627a587e13cc90d94d1377fcc7dd4eb9a71978bb
Amy Lukima <amylukima@gmail.com>
2013-08-23 00:02:07 -0700

    unrefactored problem 4 solution

--- Problem_4.rb
+++ Problem_4.rb
@@ -1,22 +1,30 @@
-#now we are going to defind a method that starts at 999 and checks for palindromes using downto
-#http://www.ruby-doc.org/core-2.0/Integer.html#method-i-downto
+=begin
+While the assumuption that starting at 999 and working out way down was good, 
+it didn't quite work the way we wanted. a maybe have been 999 the entire time, but b could have 7. 
+Lets try holding all the palindromes and seeing what we get. 
+=end
+
+
 
 def largest_product 
+	palindromes = []
 	999.downto(100).each do |a|
 		999.downto(100).each do |b|
-	c = a * b
-	return c if is_palindrome(c) 
+			c = a * b 
+			palindromes << c if is_palindrome(c)
 		end
 	end
+	 largest_palindrome(palindromes)
+end
+
+def largest_palindrome(p)
+	p.sort.reverse.first
 end
 
 def is_palindrome(number)
-	#why are we converting an interger to a string here?
-	#what other method can we use?
 	number.to_s == number.to_s.reverse
 end
 
 puts largest_product
 
-# we get an answer, but it's not right. 580085 is a palindrome, 
-# and it's the closest one to a * b
+
