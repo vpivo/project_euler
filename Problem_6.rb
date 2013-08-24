2f6b22a0482e81bee82a80b461d41d78ea262626
Amy Lukima <amylukima@gmail.com>
2013-08-24 02:23:56 -0700

    Problem 6 solution

--- Problem_6.rb
+++ Problem_6.rb
@@ -1,14 +1,25 @@
 =begin 
-The sum of the squares of the first ten natural numbers is,
-
-12 + 22 + ... + 102 = 385
-The square of the sum of the first ten natural numbers is,
-
-(1 + 2 + ... + 10)2 = 552 = 3025
-Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-
-Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+This is pretty simple. So lets just code it out. Start with defining the methods we'll need. 
 =end
+
+
+
+def sum_of_squares(n)
+	squares = (1..n).collect { |n| n * n}
+	squares.inject(:+)
+end
+
+
+def square_of_sum(n)
+	sum = (1..n).to_a.inject(:+)
+	sum * sum
+end
+
+def difference(n)
+	sum_of_squares(n) - square_of_sum(n)
+end
+
+puts difference(100)
 
 
 
